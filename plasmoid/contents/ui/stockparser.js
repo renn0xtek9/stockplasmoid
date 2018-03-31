5f2ab5b945b3027ad90cdbd240006b70eecba0c3
renn0xtek9 <renn0xtek9@laposte.net>
2018-04-01 01:09:00 +0200

    Can now post request and parse response

--- plasmoid/contents/ui/stockparser.js
+++ plasmoid/contents/ui/stockparser.js
@@ -1,8 +1,39 @@
+
+
+function httpGet()
+{
+	var theUrl="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&interval=1min&apikey=4ZM6YOXJ7O7M2JFA"
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+	xmlHttp.send( null );
+	return xmlHttp.responseText;
+}
+function AlphaVantageTimeSeriesDailyParse(jsonanswer)
+{		
+	try{
+		var obj=JSON.parse(jsonanswer)["Time Series (Daily)"];
+		var key_last_date=Object.keys(obj)[0];
+		var firstelem=obj[key_last_date];
+		var value_at_close=firstelem["4. close"];
+		var value_at_open=firstelem["1. open"];
+		var increase=value_at_close/value_at_open*100.0-100.0
+	}
+	catch(e){
+		console.log("Error while parsing answer");
+		var value_at_close="";
+		var value_at_open="";
+		var increase=0.0;
+	}
+	var ret=[value_at_close,value_at_close/value_at_open,increase];
+	return ret;
+}
+
+
 function makeList(id,list_of_tags){
 	var stringarray=list_of_tags.split(';');
 	var arraylength=stringarray.length;
 	for (var i = 0; i < arraylength; i++) {
 		id.append({"name":stringarray[i]});
 	}
-	console.log(list_of_tags);
+	console.log(AlphaVantageTimeSeriesDailyParse(httpGet()));	
 }
